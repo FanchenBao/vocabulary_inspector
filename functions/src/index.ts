dac649139f691745aea65ed6fb6f929ca8c87197
Fanchen Bao <jonathonbao@gmail.com>
2021-11-08 01:06:00 -0500

    Add functions for several Google NLP APIs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as functions from "firebase-functions";
 import * as language from "@google-cloud/language";
 import {protos} from "@google-cloud/language";
+// import * as admin from "firebase-admin";
 
 type IDocument = protos.google.cloud.language.v1.IDocument;
 
 const client = new language.LanguageServiceClient();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const analyzeSentimentString = functions.https.onRequest(
   async (request, resp) => {
     const text = request.body.text;
@@ -39,10 +38,12 @@ export const analyzeSentimentString = functions.https.onRequest(
 
 export const analyzeSentimentDocument = functions.https.onRequest(
   async (request, resp) => {
-    const text = request.body.text;
-
+    const filename = request.query.filename;
+    const bucketName = process.env.FIREBASE_CONFIG ?
+      JSON.parse(process.env.FIREBASE_CONFIG).storageBucket :
+      "";
     const document: IDocument = {
-      content: text,
+      gcsContentUri: `gs://${bucketName}/${filename}`,
       type: "PLAIN_TEXT",
     };
 
@@ -50,7 +51,6 @@ export const analyzeSentimentDocument = functions.https.onRequest(
     const [result] = await client.analyzeSentiment({document: document});
     const sentiment = result.documentSentiment;
 
-    functions.logger.log(`Text: ${text}`);
     if (sentiment) {
       functions.logger.log(`Sentiment score: ${sentiment.score}`);
       functions.logger.log(`Sentiment magnitude: ${sentiment.magnitude}`);
@@ -59,6 +59,66 @@ export const analyzeSentimentDocument = functions.https.onRequest(
         .send(
           `Sentiment = ${sentiment.score}; magnitude = ${sentiment.magnitude}`
         );
+    } else {
+      resp.status(500).send("Cannot analyze sentiment");
+    }
+  }
+);
+
+
+export const analyzeEntityDocument = functions.https.onRequest(
+  async (request, resp) => {
+    const filename = request.query.filename;
+    const bucketName = process.env.FIREBASE_CONFIG ?
+      JSON.parse(process.env.FIREBASE_CONFIG).storageBucket :
+      "";
+    const document: IDocument = {
+      gcsContentUri: `gs://${bucketName}/${filename}`,
+      type: "PLAIN_TEXT",
+    };
+
+    // Detects the sentiment of the text
+    const [result] = await client.analyzeEntities({document: document});
+    if (result.entities) {
+      const entities = result.entities
+        .sort((e1, e2) => e2.salience - e1.salience) // sort descending
+        .map((entity) => ({
+          name: entity.name,
+          type: entity.type,
+          salience: entity.salience,
+        }));
+      resp.status(200).send(JSON.stringify(entities));
+    } else {
+      resp.status(500).send("Cannot analyze sentiment");
+    }
+  }
+);
+
+export const analyzeSyntaxDocument = functions.https.onRequest(
+  async (request, resp) => {
+    const filename = request.query.filename;
+    const bucketName = process.env.FIREBASE_CONFIG ?
+      JSON.parse(process.env.FIREBASE_CONFIG).storageBucket :
+      "";
+    const document: IDocument = {
+      gcsContentUri: `gs://${bucketName}/${filename}`,
+      type: "PLAIN_TEXT",
+    };
+
+    // Detects the sentiment of the text
+    const [result] = await client.analyzeSyntax(
+      {document: document, encodeType: "UTF8"},
+    );
+    if (result.tokens) {
+      const tokens = result.tokens.map((token) => ({
+        text: token?.text?.content,
+        morphology: {
+          tag: token?.partOfSpeech?.tag,
+          case: token?.partOfSpeech?.case,
+          tense: token?.partOfSpeech?.tense,
+        },
+      }));
+      resp.status(200).send(JSON.stringify(tokens));
     } else {
       resp.status(500).send("Cannot analyze sentiment");
     }
